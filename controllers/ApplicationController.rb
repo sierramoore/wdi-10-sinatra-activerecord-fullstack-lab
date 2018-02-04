888e7d87f9986b579cb9d7e33148454dd16e1b9f
Sierra Moore <sierra.v.moore@gmail.com>
2018-02-04 15:06:29 -0600

    Basic server runs. ApplicationController + config.ru

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -1,4 +1,22 @@
 class ApplicationController < Sinatra::Base
   require 'bundler'
   Bundler.require
+
+  # ActiveRecord::Base.establish_connection(
+  #     # :adapter => 'postgresql',
+  #     # :database => ''
+  # )
+  #
+  # set :views, File.expand_path('../../views', __FILE__)
+  # set :public_dir, File.expand_path('../../public', __FILE__)
+
+
+  get '/' do
+    "Home Route"
+  end
+
+  not_found do
+    halt 404
+  end
+
 end
