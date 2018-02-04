aff505513735c14e768e7755796d3f252dba401e
Sierra Moore <sierra.v.moore@gmail.com>
2018-02-04 15:31:21 -0600

    added ActiveRecord and migrations/seeds

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -2,11 +2,11 @@ class ApplicationController < Sinatra::Base
   require 'bundler'
   Bundler.require
 
-  # ActiveRecord::Base.establish_connection(
-  #     # :adapter => 'postgresql',
-  #     # :database => ''
-  # )
-  #
+  ActiveRecord::Base.establish_connection(
+      :adapter => 'postgresql',
+      :database => 'item'
+  )
+
   set :views, File.expand_path('../../views', __FILE__)
   set :public_dir, File.expand_path('../../public', __FILE__)
 
---
+++ db/seeds.sql
@@ -0,0 +1,1 @@
+INSERT INTO users (username, password) VALUES ('Sierra', '123')
